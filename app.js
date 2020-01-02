8d01dd26c643c50dd9a6101dc47d568ea339680e
chldyddnjs <48711155+chldyddnjs@users.noreply.github.com>
2020-01-02 11:42:06 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import passport from "passport";
+import session from "express-session";
 import userRouter  from "./routers/userRouter";
 import videoRouter  from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 import { localsMiddleware } from "./middlewares";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+
+import "./passport";
+
 const app = express();
+
+const CokieStore = MongoStore();
+console.log(process.env.COOKIE_SECRET);
 
 app.use(helmet());
 app.set("view engine","pug");
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads")); //multer 사용법
+app.use("/static", express.static("static"));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(morgan("dev"));
-
+app.use(session({
+    secret : process.env.COOKIE_SECRET,
+    resave:true,
+    saveUninitialized:false,
+    store: new CokieStore({mongooseConnection:mongoose.connection})
+ }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
---
+++ userController.js
@@ -0,0 +1,48 @@
+import passport from "passport";
+import routes from "../routes"
+import User from "../models/User";
+export const getJoin = (req,res) => {
+    res.render("join",{ pageTitle: "Join"});
+};
+
+export const postJoin = async (req,res,next) => {
+    const {
+        body:{name,email,password,password2}
+    }=req;
+    if (password !== password2) {
+        res.status(400);
+        res.render("join",{ pageTitle: "Join"});
+    }else{
+        //To Do: Register User
+        try{
+        const user = await User({
+            name,
+            email
+        })
+        await User.register(user,password);
+        next();
+        } catch(error){
+            console.log(error);
+        } 
+        //To Do: Log in
+        res.redirect(routes.home);
+    }
+    
+};
+
+export const getLogin = (req,res) => 
+    res.render("login",{ pageTitle:"Log In"});
+export const postLogin = passport.authenticate('local',{
+    failureRedirect:routes.login,
+    successRedirect:routes.home
+});
+
+export const logout = (req,res) => {
+    //To Do : process Log out
+    res.redirect(routes.home);
+}
+export const users = (req,res) => res.render("Users");
+export const uesrDetail = (req,res) => res.render("userDetail");
+export const editProfile = (req,res) => res.render("editProfile");
+export const changePassword = (req,res) => res.render("changePassword");
+
